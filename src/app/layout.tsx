8bf32a1c21e9a3fa6070f8826ee88afdc589d496
shahihika <batista.oliveira@rethink.dev>
2024-04-29 18:27:01 -0300

    put back animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,17 +4,17 @@ import { useEffect } from 'react';
 import './globals.css';
 import StyledComponentsRegistry from '~/lib/registry';
 import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   
   useEffect(() => {
-    AOS.init({
-      disable: 'phone',
-    });
+    AOS.init();
   }, []);
 
   return (
@@ -22,11 +22,13 @@ export default function RootLayout({
       <link
         href='https://fonts.googleapis.com/css?family=Inter'
         rel='stylesheet'
-      ></link>
+      />
+
       <link
         href='https://fonts.googleapis.com/css?family=Public Sans'
         rel='stylesheet'
-      ></link>
+      />
+
       <body id='layout-body'>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
